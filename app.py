23192d0c5fd9176c0a8f8ecd71221a0b703b25b1
Andre Sionek <andresionek91@gmail.com>
2020-10-27 23:07:59 +0000

    adiciona DMS e Kinesis

--- app.py
+++ app.py
@@ -3,11 +3,13 @@
 from aws_cdk import core
 from common import Environment, Common
 from turma_2_data_platform.turma_2_data_platform_stack import DataLake
+from turma_2_data_platform.ingestion import RawIngestion
 import os
 
 environment = Environment[os.environ['ENVIRONMENT']]
 
 app = core.App()
 common = Common(app, environment=environment)
-DataLake(app, environment=environment)
+data_lake = DataLake(app, environment=environment)
+raw_ingestion = RawIngestion(app, data_lake=data_lake, common=common)
 app.synth()
