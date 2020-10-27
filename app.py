d9de21fc405dbfe3aa4bd71bb05dfed8cfd4c793
Andre Sionek <andresionek91@gmail.com>
2020-10-27 22:53:27 +0000

    adiciona RDS

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-from common import Environment
+from common import Environment, Common
 from turma_2_data_platform.turma_2_data_platform_stack import Turma2DataPlatformStack
 import os
 
 environment = Environment[os.environ['ENVIRONMENT']]
 
 app = core.App()
+common = Common(app, environment=environment)
 Turma2DataPlatformStack(app, environment=environment)
 app.synth()
