f214d01e56cb05374532308daf698f1b4b52c4b7
Andre Sionek <andresionek91@gmail.com>
2020-10-29 21:59:47 +0000

    adiciona stack

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from aws_cdk import core
 from common import Environment, Common
 from turma_2_data_platform.turma_2_data_platform_stack import DataLake
 from turma_2_data_platform.ingestion import RawIngestion
+from turma_2_data_platform.catalog import GlueCatalog
 import os
 
 environment = Environment[os.environ['ENVIRONMENT']]
@@ -12,4 +13,5 @@ app = core.App()
 common = Common(app, environment=environment)
 data_lake = DataLake(app, environment=environment)
 raw_ingestion = RawIngestion(app, data_lake=data_lake, common=common)
+glue_catalog = GlueCatalog(app, data_lake=data_lake)
 app.synth()
