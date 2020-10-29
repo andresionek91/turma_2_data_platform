57a00a15eb55199565e241f4031a510873be5d34
Andre Sionek <andresionek91@gmail.com>
2020-10-29 22:18:02 +0000

    cria tabela no Glue

--- turma_2_data_platform/catalog.py
+++ turma_2_data_platform/catalog.py
@@ -16,7 +16,7 @@ class GlueCatalog(core.Stack):
             f'{self.env}-atomic-events-crawler',
             name=f'{self.env}-atomic-events-crawler',
             description='Crawler to detect schema of data sored in data lake raw, atomic events',
-            schedule=glue.CfnCrawler.ScheduleProperty(schedule_expression='cron(* 15 * * ? *)'),
+            schedule=glue.CfnCrawler.ScheduleProperty(schedule_expression='cron(0/15 * * * ? *)'),
             role=data_lake.data_lake_role.role_arn,
             targets=glue.CfnCrawler.TargetsProperty(
                 s3_targets=[
@@ -25,4 +25,26 @@ class GlueCatalog(core.Stack):
                 ]
             ),
             database_name=data_lake.data_lake_raw_database.database_name
+        )
+
+        self.orders_table = glue.Table(
+            self,
+            f'{self.env}-orders-table',
+            table_name='orders',
+            description='orders captured from Postgres using DMS CDC',
+            database=data_lake.data_lake_raw_database,
+            compressed=True,
+            data_format=glue.DataFormat(
+                input_format=glue.InputFormat.TEXT,
+                output_format=glue.OutputFormat.HIVE_IGNORE_KEY_TEXT,
+                serialization_library=glue.SerializationLibrary.OPEN_CSV
+            ),
+            s3_prefix='orders',
+            bucket=data_lake.data_lake_raw_bucket,
+            columns=[
+                glue.Column(name='created_at', type=glue.Type(input_string='datetime', is_primitive=True)),
+                glue.Column(name='order_id', type=glue.Type(input_string='integer', is_primitive=True)),
+                glue.Column(name='product_name', type=glue.Type(input_string='string', is_primitive=True)),
+                glue.Column(name='value', type=glue.Type(input_string='float', is_primitive=True))
+            ]
         )
