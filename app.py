9d4774b4f68ffc68b920f84611e9ae6306f91ecc
Andre Sionek <andresionek91@gmail.com>
2020-10-29 22:27:55 +0000

    cria cluster EMR

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from common import Environment, Common
 from turma_2_data_platform.turma_2_data_platform_stack import DataLake
 from turma_2_data_platform.ingestion import RawIngestion
 from turma_2_data_platform.catalog import GlueCatalog
+from turma_2_data_platform.transform import EMRTransform
 import os
 
 environment = Environment[os.environ['ENVIRONMENT']]
@@ -14,4 +15,5 @@ common = Common(app, environment=environment)
 data_lake = DataLake(app, environment=environment)
 raw_ingestion = RawIngestion(app, data_lake=data_lake, common=common)
 glue_catalog = GlueCatalog(app, data_lake=data_lake)
+transform = EMRTransform(app, data_lake=data_lake, common=common)
 app.synth()
---
+++ turma_2_data_platform/transform.py
@@ -0,0 +1,80 @@
+from aws_cdk import core
+from turma_2_data_platform_stack import DataLake
+from common import Common
+from aws_cdk import (
+    aws_emr as emr,
+    aws_s3 as s3,
+    aws_iam as iam
+)
+
+
+class EMRTransform(core.Stack):
+
+    def __init__(self, scope: core.Construct, data_lake: DataLake, common: Common, **kwargs) -> None:
+        self.env = data_lake.env.value
+        super().__init__(scope, id=f'{self.env}-emr-transform', **kwargs)
+
+        self.logs_bucket = s3.Bucket(
+            self,
+            f'{self.env}-emr-logs-bucket',
+            bucket_name=f's3-belisco-{self.env}-emr-logs-bucket'
+        )
+
+        self.emr_role = iam.Role(
+            self,
+            f'{self.env}-emr-cluster-role',
+            assumed_by=iam.ServicePrincipal('elasticmapreduce.amazonaws.com'),
+            description='Role to allow EMR to process data',
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AmazonElasticMapReduceRole')
+            ]
+        )
+
+        self.emr_ec2_role = iam.Role(
+            self,
+            f'{self.env}-emr-ec2-role',
+            assumed_by=iam.ServicePrincipal('ec2.amazonaws.com'),
+            description='Role to allow EMR to process data',
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AmazonElasticMapReduceforEC2Role')
+            ]
+        )
+
+        self.emr_ec2_instance_profile = iam.CfnInstanceProfile(
+            self,
+            f'{self.env}-emr-instance_profile',
+            instance_profile_name=f'{self.env}-emr-instance_profile',
+            roles=[
+                self.emr_ec2_role.role_arn
+            ]
+        )
+
+        self.cluster = emr.CfnCluster(
+            self,
+            f'{self.env}-emr-cluster',
+            name=f'{self.env}-emr-cluster',
+            instances=emr.CfnCluster.JobFlowInstancesConfigProperty(
+                master_instance_group=emr.CfnCluster.InstanceGroupConfigProperty(
+                    instance_count=1,
+                    instance_type='m4.large',
+                    market='ON_DEMAND',
+                    name='Master'
+                ),
+                core_instance_group=emr.CfnCluster.InstanceGroupConfigProperty(
+                    instance_count=2,
+                    instance_type='m4.large',
+                    market='ON_DEMAND',
+                    name='Core'
+                ),
+                termination_protected=True,
+                ec2_subnet_ids=[subnet.subnet_id for subnet in common.custom_vpc.private_subnets]
+            ),
+            applications=[
+                emr.CfnCluster.ApplicationProperty(name='Spark')
+            ],
+            log_uri=f's3://{self.logs_bucket.bucket_name}/logs',
+            job_flow_role=self.emr_ec2_instance_profile.get_att('arn').to_string(),
+            service_role=self.emr_role.role_arn,
+            release_label='emr-5.30.1',
+            visible_to_all_users=True
+        )
